a5e3e654164828401b96446dad046c3a49e05823
wy_fan_wq <wy_fan_wq@163.com>
2017-11-22 10:18:09 +0800

    添加IDAL部分文件

---
+++ IDAL/Ialbum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using models;
+using System.Data;
+
+namespace IDAL
+{
+   public interface Ialbum
+    {
+        int InsertAlbum(album album);
+        int UpdateAlbum(album album);
+        DataTable SelectAlbum(string album_name);
+    }
+}
