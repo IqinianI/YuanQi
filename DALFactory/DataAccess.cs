c355c3194e2c4b94ffab141950142b0d5f5cf7a0
sam <awesome_sam@qq.com>
2017-11-28 11:05:54 +0800

    新建了Web层，为合并冲突做准备。

---
+++ DALFactory/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IDAL;
+using System.Reflection;
+using System.Configuration;
+
+namespace DALFactory
+{
+    public class DataAccess
+    {
+        private static string AssemblyName = ConfigurationManager.AppSettings["path"].ToString();
+        private static string db = ConfigurationManager.AppSettings["DB"].ToString();
+
+        public static 
+    }
+}
